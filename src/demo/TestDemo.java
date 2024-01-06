0785750e640b4bd6263f060dd6a8b6cb59be764d
noort <noort@DESKTOP-KKE0VB6>
2024-01-05 19:59:11 -0500

    added comment

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -2,6 +2,7 @@ package demo;
 
 public class TestDemo {
 
+	//Add your name --> John Cena
 	public static void main(String[] args) {
 		System.out.println("Day 2 Git..");
 		System.out.println("Exercise1");
